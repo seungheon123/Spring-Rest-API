c0492afa95120b791325aca7280586e8696b61e0
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-09-28 21:04:44 +0900

    api 생성

---
+++ src/main/java/hoho/test/api/MemberController.java
@@ -0,0 +1,14 @@
+package hoho.test.api;
+
+
+import hoho.test.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+}
