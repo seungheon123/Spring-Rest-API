95b6aa9816564e4e743e35b83c6cdf1e106f5823
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-09-29 19:18:50 +0900

    withdraw 메서드 생성

--- src/main/java/hoho/test/service/MemberService.java
+++ src/main/java/hoho/test/service/MemberService.java
@@ -22,10 +22,25 @@ public class MemberService {
         return member.getId();
     }
 
+    @Transactional
+    public void withdraw(Member member){
+        Member newMember = checkExistMember(member);
+        memberRepository.delete(newMember);
+    }
+
+    private Member checkExistMember(Member member) {
+        Member findMember = memberRepository.findOne(member.getId());
+        if(findMember==null) throw new IllegalStateException("존재하지 않는 회원입니다");
+        else return findMember;
+    }
+
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getName());
         if(!findMembers.isEmpty()) throw new IllegalStateException("이미 존재하는 회원입니다");
     }
+
+
+
 
 
 }
