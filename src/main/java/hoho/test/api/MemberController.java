0c656f1b1733b6be7c08d25b9c82f09e5c5beb00
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-09-29 19:16:52 +0900

    회원 삭제 API 생성

--- src/main/java/hoho/test/api/MemberController.java
+++ src/main/java/hoho/test/api/MemberController.java
@@ -3,10 +3,12 @@ package hoho.test.api;
 
 import hoho.test.domain.Member;
 import hoho.test.dto.MemberJoinDto;
+import hoho.test.dto.MemberWithdrawDto;
 import hoho.test.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,19 @@ public class MemberController {
             return new ResponseEntity<>(id,HttpStatus.OK);
         }catch (IllegalStateException e){
             return new ResponseEntity<>("에러가 발생했습니다", HttpStatus.CONFLICT);
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteUser(@RequestBody MemberWithdrawDto userDto){
+        try{
+            Member member = new Member();
+            member.setId(userDto.getId());
+            member.setName(userDto.getName());
+            memberService.withdraw(member);
+            return new ResponseEntity<>("Success",HttpStatus.OK);
+        }catch (IllegalStateException e){
+            return new ResponseEntity<>("에러가 발생했습니다",HttpStatus.CONFLICT);
         }
     }
 
