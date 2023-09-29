30ac928da44ea45cc0a3f3272beb047941d78c3d
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-09-29 18:40:39 +0900

    회원 가입 API 생성

--- src/main/java/hoho/test/api/MemberController.java
+++ src/main/java/hoho/test/api/MemberController.java
@@ -1,14 +1,33 @@
 package hoho.test.api;
 
 
+import hoho.test.domain.Member;
+import hoho.test.dto.MemberJoinDto;
 import hoho.test.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+
+    @PostMapping("/join")
+    public ResponseEntity<?> createUser(@RequestBody MemberJoinDto userDto){
+        try{
+            Member member = new Member();
+            member.setName(userDto.getName());
+            member.setAddress(userDto.getAddress());
+            Long id = memberService.join(member);
+            return new ResponseEntity<>(id,HttpStatus.OK);
+        }catch (IllegalStateException e){
+            return new ResponseEntity<>("에러가 발생했습니다", HttpStatus.CONFLICT);
+        }
+    }
+
 
 }
