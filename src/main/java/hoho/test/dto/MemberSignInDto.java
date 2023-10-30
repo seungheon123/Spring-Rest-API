2906e3308aeae85b8490526e14761f5ee33fcdd4
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-10-30 13:39:19 +0900

    로그인 Dto 생성

--- src/main/java/hoho/test/dto/MemberSignInDto.java
+++ src/main/java/hoho/test/dto/MemberSignInDto.java
@@ -1,2 +1,10 @@
-package hoho.test.dto;public class MemberSignInDto {
+package hoho.test.dto;
+
+import lombok.Data;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+@Data
+public class MemberSignInDto {
+    private String name;
+
 }
