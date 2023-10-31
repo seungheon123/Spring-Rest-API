180c7b29686f985a48ab750b971f311010270057
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-10-31 15:28:58 +0900

    Swagger Configuration

--- src/main/java/hoho/test/config/SwaggerConfig.java
+++ src/main/java/hoho/test/config/SwaggerConfig.java
@@ -1,38 +1,26 @@
 package hoho.test.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket swagger(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .ignoredParameterTypes(java.sql.Date.class)
-                .forCodeGeneration(true)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("test.api"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo())
-                .enable(true);
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("테스트 API")
-                .description("테스트 API 설명 및 사용법")
-                .version("1.0")
-                .build();
+    private Info apiInfo() {
+        return new Info()
+                .title("TEST API")
+                .description("테스트 API 명세서")
+                .version("1.0.0");
     }
 
 }
